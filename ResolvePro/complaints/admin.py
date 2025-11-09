508bcf4d432c5deee57e3229d482f7fa1763052c
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2025-11-09 16:39:59 +0530

    registered vendor

--- ResolvePro/complaints/admin.py
+++ ResolvePro/complaints/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Client,Job,Category,Resident
+from vendors.models import Vendor
 
 admin.site.register(Resident)
 admin.site.register(Category)
 admin.site.register(Client)
 admin.site.register(Job)
+admin.site.register(Vendor)
