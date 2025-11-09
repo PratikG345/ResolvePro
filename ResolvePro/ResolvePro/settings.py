38b592944ea1fa7a0667f8257a71e756c370057c
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2025-11-09 16:46:27 +0530

    Ready for deployment

--- ResolvePro/ResolvePro/settings.py
+++ ResolvePro/ResolvePro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$&18%hzrihq#%ms%n9fc4pzehj(oa-2*s#ms1b6jr*v89h5!q4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
