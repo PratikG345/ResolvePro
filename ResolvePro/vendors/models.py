fe1cfe58dc6579c3291e060608a234e67b02cc9c
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2025-11-09 16:41:05 +0530

    small change

--- ResolvePro/vendors/models.py
+++ ResolvePro/vendors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from complaints.models import Client,Category
 # Create your models here.
+
+class Vendor(models.Model):
+    
+    name = models.CharField(max_length=100)
+    comtact_no = models.CharField(max_length=100)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    def __str__(self):
+        return f"{self.name} - {self.category.name}"
