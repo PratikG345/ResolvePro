01b30ede78c8718d09b5f5fc2803e36403cbbfab
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2025-11-09 16:40:19 +0530

    added assigned vendor field

--- ResolvePro/complaints/models.py
+++ ResolvePro/complaints/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from clients.models import Client
 
+
 # Create your models here.
 class Resident(models.Model):
     flat_no = models.CharField(max_length=20)
@@ -26,6 +27,7 @@ class Job(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.TextField()
     photo = models.ImageField(upload_to="complaintphotos/")
+    assigned_vendor = models.ForeignKey('vendors.Vendor', on_delete=models.SET_NULL, null=True, blank=True)
     status = models.CharField(max_length=100,choices=STATUS_CHOICES, default="open")
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
