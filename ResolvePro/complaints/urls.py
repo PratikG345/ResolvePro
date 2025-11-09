9ccad806fed8713734b0117a78235b3e74f1dc78
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2025-11-09 16:40:41 +0530

    done

--- ResolvePro/complaints/urls.py
+++ ResolvePro/complaints/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('complaint/',views.complaints),
-    path('dashboard/',views.dashboard),
+    path('dashboard/',views.dashboard, name='dashboard'),
+    path('assign_vendor/<int:job_id>/', views.assign_vendor, name='assign_vendor'),
+    path('mark_resolved/<int:job_id>/', views.mark_resolved, name='mark_resolved'),
     path('admin/', admin.site.urls),
 ]
--- ResolvePro/complaints/views.py
+++ ResolvePro/complaints/views.py
@@ -1,13 +1,34 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Job,Resident,Category,Client
+from vendors.models import Vendor
 # Create your views here.
 
 def dashboard(req):
+    if req.GET.get('secret') != 'akash123':  # Change this password
+        return HttpResponse('Unauthorized - Secretaries only', status=401)
     client = Client.objects.first()
-    return render(req,"complaints/dashboard.html",{'client':client})
+    jobs = Job.objects.all()
+    vendors = Vendor.objects.filter(is_active=True)
+    open = Job.objects.filter(status='OPEN').count()
+    assigned = Job.objects.filter(status='ASSIGNED').count()
+    resolved = Job.objects.filter(status='RESOLVED').count()
+    vendor_name = Vendor.objects.filter(name='')
+    context = {
+        'client':client,
+        'jobs':jobs,
+        'vendors':vendors,
+        'open':open,
+        'assigned':assigned,
+        'resolved':resolved,
+        
+    }
+    return render(req,"complaints/dashboard.html",context)
 
 def complaints(request):
+    client = Client.objects.first()
+    show_success = False 
+    
     if request.method == 'POST':
         # 1. EXTRACT THE DATA
         flat_no = request.POST['flat_no']
@@ -17,7 +38,6 @@ def complaints(request):
         photo = request.FILES.get('photo')
         
         # 2. GET THE CLIENT (hardcode for now)
-        client = Client.objects.first()
         
         # 3. FIND OR CREATE RESIDENT
         resident, created = Resident.objects.get_or_create(
@@ -37,12 +57,29 @@ def complaints(request):
             status='OPEN'
         )
         
-        return HttpResponse("Complaint submitted successfully!")  # Temporary
-        
+        show_success = True        
     # GET request - show the empty form
     client = Client.objects.first()
     categories = Category.objects.filter(client=client)
     return render(request, 'complaints/job_form.html', {
         'client': client,
-        'categories': categories
+        'categories': categories,
+        'show_success': show_success
     })
+    
+def assign_vendor(req,job_id):
+    if req.method == 'POST':
+        job = Job.objects.get(id=job_id)
+        vendor_id = req.POST['vendor_id']
+        job.assigned_vendor_id = vendor_id
+        job.status = 'ASSIGNED'
+        job.save()
+        print(f"Assigned {job.id} to vendor {job.assigned_vendor}")
+        return redirect('dashboard')
+    
+def mark_resolved(request, job_id):
+    if request.method == 'POST':
+        job = Job.objects.get(id=job_id)
+        job.status = 'RESOLVED'
+        job.save()
+        return redirect('dashboard')
